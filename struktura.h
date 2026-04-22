1ae1c6a52eb9ac4b065aa56d7dd41b99630e7635
KasparasStep <kasparasstep@gmail.com>
2026-04-22 20:01:44 +0300

    set egz

--- struktura.h
+++ struktura.h
@@ -91,6 +91,7 @@ double GalMed() const { return gal_med_; }
 void Vardas(const string& v) { vardas_ = v; }
 void Pavarde(const string& p) { pavarde_ = p; }
 void Paz(const vector<int>& paz) { paz_ = paz; }
+void Egz(int egz) { egz_ = egz; }
 
 // Funkcijų prototipai
 int gautiSkaiciu(string info, int min, int max);
